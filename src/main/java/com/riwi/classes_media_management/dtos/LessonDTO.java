4ab2a559f1011382373c345dce65974793717106
daguttt <dagutmu667@gmail.com>
2024-08-30 19:30:41 -0500

    refactor: remove unused imports

--- src/main/java/com/riwi/classes_media_management/dtos/LessonDTO.java
+++ src/main/java/com/riwi/classes_media_management/dtos/LessonDTO.java
@@ -1,12 +1,10 @@
 package com.riwi.classes_media_management.dtos;
 
 import com.riwi.classes_media_management.enums.MediaTypes;
-import jakarta.persistence.Column;
 import lombok.Builder;
 import lombok.Data;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.URL;
 
 @Data
