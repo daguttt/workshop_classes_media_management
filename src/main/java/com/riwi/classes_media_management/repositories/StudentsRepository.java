93cc912588ecfece1961c0af9ad5f4ac562b1790
BUKODONOZOR <jafetdavidibarguenlozano@pascualbravo.edu.co>
2024-08-31 12:09:16 -0500

    create student repository

--- src/main/java/com/riwi/classes_media_management/repositories/StudentsRepository.java
+++ src/main/java/com/riwi/classes_media_management/repositories/StudentsRepository.java
@@ -1,5 +1,17 @@
 package com.riwi.classes_media_management.repositories;
 
-public interface StudentsRepository {
+import com.riwi.classes_media_management.entities.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    // Method to find a student by e-mail
+    Optional<Student> findByEmail(String email);
+
+    // Method to find all active students
+    List<Student> findByActiveTrue();
 }
