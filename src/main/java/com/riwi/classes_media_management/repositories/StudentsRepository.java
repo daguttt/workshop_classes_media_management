973339c7efc4385462a9bce978f39da90a91b44e
daguttt <dagutmu667@gmail.com>
2024-08-31 14:27:34 -0500

    fix: import List interface and add missing 's' for StudentsRepository.java

--- src/main/java/com/riwi/classes_media_management/repositories/StudentsRepository.java
+++ src/main/java/com/riwi/classes_media_management/repositories/StudentsRepository.java
@@ -4,10 +4,11 @@ import com.riwi.classes_media_management.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
+public interface StudentsRepository extends JpaRepository<Student, Long> {
 
     // Method to find a student by e-mail
     Optional<Student> findByEmail(String email);
