67e5fdfec155e8dda388806c43355de73a9514f4
daguttt <dagutmu667@gmail.com>
2024-09-01 11:37:11 -0500

    refactor: remove redeclaration of the `findById()` in ClassRepository

--- src/main/java/com/riwi/classes_media_management/repositories/ClassRepository.java
+++ src/main/java/com/riwi/classes_media_management/repositories/ClassRepository.java
@@ -5,12 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface ClassRepository extends JpaRepository<ClassEntity, Long> {
 
     List<ClassEntity> findByActiveTrue();
 
-    Optional<ClassEntity> findById(Long id);
 }
