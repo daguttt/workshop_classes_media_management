67bd8d59caf86e854c61bdbe33938a121ae9cca1
Daniel Gutierrez Muñoz <70865733+daguttt@users.noreply.github.com>
2024-09-01 11:49:49 -0500

    feat: validate lesson file based on media type (#8)

--- src/main/java/com/riwi/classes_media_management/services/LessonsService.java
+++ src/main/java/com/riwi/classes_media_management/services/LessonsService.java
@@ -5,13 +5,17 @@ import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.riwi.classes_media_management.dtos.LessonDTO;
 import com.riwi.classes_media_management.entities.Lesson;
 import com.riwi.classes_media_management.enums.MediaTypes;
 import com.riwi.classes_media_management.services.interfaces.ILessonsService;
+import com.riwi.classes_media_management.storage.FileFormatValidator;
 import com.riwi.classes_media_management.storage.IStorageService;
+import com.riwi.classes_media_management.storage.exceptions.StorageException;
 
 @Service
 public class LessonsService implements ILessonsService {
@@ -20,10 +24,29 @@ public class LessonsService implements ILessonsService {
   @Autowired
   private IStorageService storageService;
 
+  @Autowired
+  private FileFormatValidator fileFormatValidator;
+
   @Override
   public Lesson create(LessonDTO lessonDTO) {
+    // Validate mediaType as document
+    var isMediaTypeDocument = lessonDTO.getMediaType().equals(MediaTypes.DOCUMENT);
+    var hasContentProperty = !lessonDTO.getContent().isEmpty();
+    var isFileEmpty = lessonDTO.getFile().isEmpty();
+
+    if (!isFileEmpty && isMediaTypeDocument && hasContentProperty)
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Uploading file when 'mediaType' is 'DOCUMENT'");
+
+    // Validate mediaType as Video or Audio
+    var isVideoOrAudioFormatValid = fileFormatValidator.validate(lessonDTO.getFile(), lessonDTO.getMediaType());
+    boolean isFileValid = (isMediaTypeDocument && hasContentProperty)
+        || isVideoOrAudioFormatValid;
+
+    if (!isFileValid)
+      throw new StorageException("Invalid file format for the selected media type");
+
     String fileUrl = null;
-    boolean uplodingAudioOrVideo = !lessonDTO.getMediaType().equals(MediaTypes.DOCUMENT);
+    boolean uplodingAudioOrVideo = !isMediaTypeDocument;
     // Store file in case of audios or videos
     if (uplodingAudioOrVideo) {
       try {
