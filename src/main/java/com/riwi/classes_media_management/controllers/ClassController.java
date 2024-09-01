78e5d6011e65c6a10cfc38bcfa3539944b55a282
daguttt <dagutmu667@gmail.com>
2024-09-01 11:53:58 -0500

    feat: change ClassController base mapping to be REST compliant

--- src/main/java/com/riwi/classes_media_management/controllers/ClassController.java
+++ src/main/java/com/riwi/classes_media_management/controllers/ClassController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/class")
+@RequestMapping("/api/v1/classes")
 public class ClassController {
 
     private final ClassService classService;
