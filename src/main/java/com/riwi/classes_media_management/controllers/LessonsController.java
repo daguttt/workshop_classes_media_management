b2fcd7cafda46f0e7d4732c7f81ea35fef04160f
daguttt <dagutmu667@gmail.com>
2024-09-01 22:24:51 -0500

    feat: document lesson creation endpoint

--- src/main/java/com/riwi/classes_media_management/controllers/LessonsController.java
+++ src/main/java/com/riwi/classes_media_management/controllers/LessonsController.java
@@ -8,6 +8,12 @@ import com.riwi.classes_media_management.dtos.LessonDisabledDTO;
 import com.riwi.classes_media_management.dtos.LessonReponseDTO;
 import com.riwi.classes_media_management.services.interfaces.ILessonsService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +32,23 @@ public class LessonsController {
   @Autowired
   private ILessonsService lessonsService;
 
+  @Operation(summary = "Create a class' lesson")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "201", description = "Lesson successfully created", content = {
+          @Content(mediaType = "application/json", schema = @Schema(implementation = LessonReponseDTO.class))
+      }),
+      @ApiResponse(responseCode = "404", description = "`classId` not found", content = {
+          @Content(mediaType = "application/json")
+      }),
+      @ApiResponse(responseCode = "400", description = "Invalid file format", content = {
+          @Content(mediaType = "application/json")
+      }),
+  })
+  @RequestBody(description = "Schema to create a Lesson.\n"
+      + "- When *'DOCUMENT'* `mediaType` is selected, the `content` field is **required** and `file` field should be empty.\n"
+      + "- When *'AUDIO'* or *'VIDEO'* `mediaType` is selected, the `file` field is **required** and the `content` field should be empty.", content = {
+          @Content(mediaType = "multipart/form-data", schema = @Schema(implementation = LessonDTO.class))
+      })
   @PostMapping
   public ResponseEntity<LessonReponseDTO> create(@Valid @ModelAttribute LessonDTO lessonDTO) {
     return ResponseEntity.status(HttpStatus.CREATED).body(this.lessonsService.create(lessonDTO));
