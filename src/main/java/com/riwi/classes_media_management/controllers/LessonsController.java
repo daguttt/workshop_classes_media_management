0d3c93b721a5f667288a790bc8abb5b54c350171
daguttt <dagutmu667@gmail.com>
2024-10-02 23:59:51 -0500

    refactor: format `LessonsController.java`

--- src/main/java/com/riwi/classes_media_management/controllers/LessonsController.java
+++ src/main/java/com/riwi/classes_media_management/controllers/LessonsController.java
@@ -29,55 +29,48 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequestMapping("/api/v1/lessons")
 public class LessonsController {
 
-  @Autowired
-  private ILessonsService lessonsService;
+    @Autowired
+    private ILessonsService lessonsService;
 
-  @Operation(summary = "Create a class' lesson")
-  @ApiResponses(value = {
-      @ApiResponse(responseCode = "201", description = "Lesson successfully created", content = {
-          @Content(mediaType = "application/json", schema = @Schema(implementation = LessonReponseDTO.class))
-      }),
-      @ApiResponse(responseCode = "400", description = "Invalid file format", content = {
-          @Content(mediaType = "application/json")
-      }),
-      @ApiResponse(responseCode = "404", description = "`classId` not found", content = {
-          @Content(mediaType = "application/json")
-      }),
-  })
-  @RequestBody(description = "Schema to create a Lesson.\n"
-      + "- When *'DOCUMENT'* `mediaType` is selected, the `content` field is **required** and `file` field should be empty.\n"
-      + "- When *'AUDIO'* or *'VIDEO'* `mediaType` is selected, the `file` field is **required** and the `content` field should be empty.", content = {
-          @Content(mediaType = "multipart/form-data", schema = @Schema(implementation = LessonDTO.class))
-      })
-  @PostMapping
-  public ResponseEntity<LessonReponseDTO> create(@Valid @ModelAttribute LessonDTO lessonDTO) {
-    return ResponseEntity.status(HttpStatus.CREATED).body(this.lessonsService.create(lessonDTO));
-  }
+    @Operation(summary = "Create a class' lesson")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Lesson successfully created",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = LessonReponseDTO.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid file format",
+                    content = { @Content(mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", description = "`classId` not found",
+                    content = { @Content(mediaType = "application/json") }), })
+    @RequestBody(description = "Schema to create a Lesson.\n"
+            + "- When *'DOCUMENT'* `mediaType` is selected, the `content` field is **required** and `file` field should be empty.\n"
+            + "- When *'AUDIO'* or *'VIDEO'* `mediaType` is selected, the `file` field is **required** and the `content` field should be empty.",
+            content = {
+                    @Content(mediaType = "multipart/form-data", schema = @Schema(implementation = LessonDTO.class)) })
+    @PostMapping
+    public ResponseEntity<LessonReponseDTO> create(@Valid @ModelAttribute LessonDTO lessonDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.lessonsService.create(lessonDTO));
+    }
 
-  @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "Lesson successfully disabled", content = {
-          @Content(mediaType = "application/json", schema = @Schema(implementation = LessonDisabledDTO.class))
-      }),
-      @ApiResponse(responseCode = "404", description = "Lesson's `id` not found", content = {
-          @Content(mediaType = "application/json")
-      }),
-  })
-  @PatchMapping("/{id}/disable")
-  public ResponseEntity<LessonDisabledDTO> disable(@PathVariable Long id) {
-    return ResponseEntity.ok(this.lessonsService.disable(id));
-  }
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lesson successfully disabled",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = LessonDisabledDTO.class)) }),
+            @ApiResponse(responseCode = "404", description = "Lesson's `id` not found",
+                    content = { @Content(mediaType = "application/json") }), })
+    @PatchMapping("/{id}/disable")
+    public ResponseEntity<LessonDisabledDTO> disable(@PathVariable Long id) {
+        return ResponseEntity.ok(this.lessonsService.disable(id));
+    }
 
-  @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "Lesson successfully fetched", content = {
-          @Content(mediaType = "application/json", schema = @Schema(implementation = LessonReponseDTO.class))
-      }),
-      @ApiResponse(responseCode = "404", description = "Lesson's `id` not found", content = {
-          @Content(mediaType = "application/json")
-      }),
-  })
-  @GetMapping("/{id}")
-  public ResponseEntity<LessonReponseDTO> getById(@PathVariable Long id) {
-    return ResponseEntity.ok(this.lessonsService.getById(id));
-  }
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lesson successfully fetched",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = LessonReponseDTO.class)) }),
+            @ApiResponse(responseCode = "404", description = "Lesson's `id` not found",
+                    content = { @Content(mediaType = "application/json") }), })
+    @GetMapping("/{id}")
+    public ResponseEntity<LessonReponseDTO> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(this.lessonsService.getById(id));
+    }
 
 }
